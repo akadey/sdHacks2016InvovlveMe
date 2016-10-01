6af60f77fc0525e81f87b096a10f9c451e6fc66b
Gates Zeng <gatesazeng@gmail.com>
2016-10-01 16:33:15 -0700

    created profile

--- app/src/main/java/com/gateszeng/sdhacks2016/Person.java
+++ app/src/main/java/com/gateszeng/sdhacks2016/Person.java
@@ -24,7 +24,7 @@ public class Person {
 
     public String getHomePhone() { return homePhone; }
 
-    public String getCellPhonePhone() { return cellPhone; }
+    public String getCellPhone() { return cellPhone; }
 
     public String getWorkPhone() { return workPhone; }
 
